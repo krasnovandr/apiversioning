45de8fa76838c09af1d39668778b4de5e14a9581
Aliaksandr Anufryieu <x.anufriev@gmail.com>
2018-02-13 13:07:28 +0300

    add new web job on .net 4.7.1

---
+++ TestVersionApi/WebJob/Functions.cs
@@ -0,0 +1,45 @@
+﻿namespace TestWebJob
+{
+    using System.IO;
+    using System.Text;
+
+    using Microsoft.AspNetCore.DataProtection;
+    using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+    using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Functions
+    {
+        private static ServiceProvider serviceProvider;
+
+        static Functions()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDataProtection()
+                .UseCryptographicAlgorithms(
+                    new AuthenticatedEncryptorConfiguration
+                        {
+                            EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+                            ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+                        })
+                .SetApplicationName("testapp");
+
+            serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([ServiceBusTrigger("%TopicName%", "%SubscriptionName%")] byte[] message, TextWriter log)
+        {
+            var dataProtectionProvider = serviceProvider.GetService<IDataProtectionProvider>();
+
+            var dataProtector = dataProtectionProvider.CreateProtector("CreateProtector");
+
+            var unprotect = dataProtector.Unprotect(message);
+
+            log.WriteLine(Encoding.UTF8.GetString(unprotect));
+        }
+    }
+}
---
+++ TestVersionApi/WebJob/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Configuration;
+
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob
+{
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            CreateSubscription();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+        
+        private static void CreateSubscription()
+        {
+            var nameSpace = NamespaceManager.CreateFromConnectionString(ConfigurationManager
+                .ConnectionStrings["AzureWebJobsServiceBus"].ConnectionString);
+
+            var topicName = ConfigurationManager.AppSettings["TopicName"];
+            var version = ConfigurationManager.AppSettings["Version"];
+            var subscriptionName = ConfigurationManager.AppSettings["SubscriptionName"];
+
+            if (nameSpace.SubscriptionExists(topicName, subscriptionName) == false)
+            {
+                var filter = new SqlFilter($"Version = '{version}'");
+
+                nameSpace.CreateSubscription(topicName, subscriptionName, filter);
+            }
+
+        }
+    }
+}
